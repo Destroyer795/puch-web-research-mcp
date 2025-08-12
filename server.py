4481e0803f03e4731357548bcb2b6fe7683e28d3
Pranav Kishan <149817232+Destroyer795@users.noreply.github.com>
2025-08-12 21:32:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-# server.py
 import asyncio
 from main import app, PORT
 
 async def run_server():
-    """A robust function to run the server."""
     print(f"Starting server on host 0.0.0.0 and port {PORT}")
     await app.run_async(transport="http", host="0.0.0.0", port=PORT)
 
